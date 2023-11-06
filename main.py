d6cdd933d7fa7ea977565dc6ef143bbc170f76d4
huud-e <82060436+huud-e@users.noreply.github.com>
2023-11-06 16:58:07 +0100

    feat: trying to get a common packet structure for analyce

--- main.py
+++ main.py
@@ -1,34 +1,78 @@
-"""
-The intention of this program is to visualize and understand all the traffic in
-a network. By creating a map:
-    Fase 1: Non-interactive map, maybe just an array and some form of displaying
-    the traffic
-    Fase 2: Interactive map with all the data accessible to the user to evaluate.
-"""
-import pyshark
 import socket
+import struct
+import subprocess
+import sys
 
-def main():
-    """
-    This will be the main function and will descrive the program and how to use it.
-    """
-    capture = pyshark.LiveCapture(interface='wlan0')
-    for packet in capture.sniff_continuously(packet_count=10):
-        output(packet)
+def analyze_packet(packet):
+    # Extract Ethernet header information
+    eth_header = packet[:14]
+    eth_header = struct.unpack("!6s6sH", eth_header)
+    src_mac = ":".join([hex(x)[2:].zfill(2) for x in eth_header[0]])
+    dst_mac = ":".join([hex(x)[2:].zfill(2) for x in eth_header[1]])
+    eth_type = socket.ntohs(eth_header[2])
 
-def output(packet):
-    """
-    This function will define the temporary output and filtering of the desired 
-    data of the packet.
-    """
+    print(f"Source MAC: {src_mac}  Destination MAC: {dst_mac}  EtherType: {hex(eth_type)}")
 
-    ip_proto={v:k[8:] for (k,v) in vars(socket).items() if k.startswith('IPPROTO')}
-    ip_protocol = ip_proto.get(packet.ip.proto)
-    ip_src = packet.ip.src
-    ip_dst = packet.ip.dst
+    # Extract IP header information
+    if eth_type == 0x0800:  # IPv4
+        ip_header = packet[14:34]
+        iph = struct.unpack("!BBHHHBBH4s4s", ip_header)
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+        iph_length = ihl * 4
+        ttl = iph[5]
+        protocol = iph[6]
+        src_ip = socket.inet_ntoa(iph[8])
+        dst_ip = socket.inet_ntoa(iph[9])
 
-    print("ip_protocol", ip_protocol, "ip_src", ip_src, "ip_dst", ip_dst)
+        print(f"Source IP: {src_ip}  Destination IP: {dst_ip}  Protocol: {protocol}")
+
+    # Add more packet analysis code for other protocols as needed
+
+def start_packet_capture(interface):
+    # Create a raw socket to capture packets
+    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+
+    # Bind the socket to the specified interface
+    sock.bind((interface, 0))
+
+    # Set socket options to receive all packets
+    sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # Start capturing packets
+    while True:
+        try:
+            # Receive a packet
+            packet, _ = sock.recvfrom(65535)
+
+            # Process the packet
+            analyze_packet(packet)
+
+        except KeyboardInterrupt:
+            break
+
+    # Disable promiscuous mode and close the socket
+    sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+    sock.close()
 
 
-if __name__ == '__main__':
-    main()
+
+def find_wireless_interface():
+    encoding = sys.getdefaultencoding()  # Get the system's default encoding
+    output = subprocess.check_output("netsh wlan show interfaces")
+    output = output.decode(encoding, errors='ignore')  # Ignore decoding errors
+    lines = output.splitlines()
+    interface_name = None
+
+    for line in lines:
+        if "Name" in line:
+            _, interface_name = line.split(":")
+            interface_name = interface_name.strip()
+
+    return interface_name
+
+
+# Start the packet capture on a specific network interface
+interface = find_wireless_interface()  # Find the wireless interface name
+start_packet_capture(interface)
